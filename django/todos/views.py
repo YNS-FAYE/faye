be68072562c816404383870e920b1470f4d9ce9d
davetorrente <dave.torrente@gmail.com>
2018-10-17 16:37:36 +0800

    project setup django-react boilerplate

---
+++ django/todos/views.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# todos/views.py
+from rest_framework import generics
+
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class ListTodo(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
